c5ecc9481552e6b762d44d8fb9414b2a4851371a
Kailash Rawat <rawatkailash7188@gmail.com>
2026-05-22 17:05:58 +0530

    Update Feature-1.py

--- Feature-1.py
+++ Feature-1.py
@@ -1,2 +1,5 @@
 if __name__ == "__main__":
     print("This is the main entry point of the program.")
+print(
+    "This code will only run if this file is executed directly, not if it's imported as a module."
+)
